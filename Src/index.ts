a628d555dca6bbaf8c78e3498a2a17520f7f71b1
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-10-17 21:10:20 +0200

    First_commit

---
+++ Src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+const app: Application = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World !!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Is Running On Port ${PORT}`);
+});
+
+export default app;
